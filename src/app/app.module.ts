7f1ba1e6c9074db7966c1873148f40330853ca67
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-10-23 00:05:44 -0600

    portafolioAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ViewMainComponent } from './components/view-main/view-main.component';
+import { ViewTwoComponent } from './components/view-two/view-two.component';
+import { ViewThreeComponent } from './components/view-three/view-three.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ViewMainComponent,
+    ViewTwoComponent,
+    ViewThreeComponent
   ],
   imports: [
     BrowserModule,
