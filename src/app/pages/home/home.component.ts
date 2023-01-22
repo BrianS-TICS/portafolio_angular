d807159310c3fcbb90fbb6055d8b192a5332451b
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2023-01-22 14:56:23 -0700

    Se modifican algunos estilos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CitasTextualesService } from 'src/app/services/citas-textuales.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss', './../../app.component.scss']
+  styleUrls: ['./home.component.scss', './../../app.component.scss', './../../../styles.scss']
 })
 export class HomeComponent implements OnInit {
 
