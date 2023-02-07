6e1157eafe3e64fcf588ef35f0fd9da7d4a3aca0
BrianS-TICS <briansanchezaguilar@gmail.com>
2023-02-06 21:11:31 -0700

    Se agrega creacion de citas

--- src/app/pages/administration/authenticated/quotes/quotes.component.ts
+++ src/app/pages/administration/authenticated/quotes/quotes.component.ts
@@ -10,19 +10,25 @@ import { CitasTextualesService } from 'src/app/services/citas-textuales.service'
 export class QuotesComponent implements OnInit {
 
   public autorForm: FormGroup;
+  public citasForm: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
+    private fb2: FormBuilder,
+
     private citasTextService: CitasTextualesService
   ) {
     this.autorForm = this.initAuthorForm();
+    this.citasForm = this.initCitasForm();
   }
 
   public sectionActive = 'authors'
-  public autores = null;
+  public autores = [];
+  public citas = [];
 
   ngOnInit(): void {
     this.obtenerAutores();
+    this.obtenerCitas();
   }
 
   public changeSection(section: any) {
@@ -39,6 +45,7 @@ export class QuotesComponent implements OnInit {
       {
         next: (response) => {
           this.autores.push(response.data);
+
           this.autorForm = this.initAuthorForm();
         },
         error: (e) => {
@@ -68,10 +75,57 @@ export class QuotesComponent implements OnInit {
 
   }
 
+
+  public obtenerCitas() {
+
+    this.citasTextService.obtenerCitas().subscribe(
+      {
+        next: (response) => {
+          this.citas = response.data
+          console.log(this.citas);
+        },
+        error: (e) => {
+          console.log(e);
+        }
+      }
+    )
+
+  }
+
+  public crearCita() {
+
+    const cita = this.citasForm.getRawValue();
+
+    this.citasTextService.crearCita(cita).subscribe(
+      {
+        next: (response) => {
+          if (this.citas.length > 0) {
+            this.citas.push(response.data)
+          } else {
+            this.citas = [response.data]
+          }
+          console.log(response.data);
+          this.citasForm.reset();
+        },
+        error: (e) => {
+          console.log(e);
+        }
+      }
+    )
+  }
+
   public initAuthorForm() {
     return this.autorForm = this.formBuilder.group({
       nombre: new FormControl({ value: null, disabled: false }, [Validators.required]),
       apellidos: new FormControl({ value: null, disabled: false }, [Validators.required]),
+    });
+  }
+
+  public initCitasForm() {
+    return this.citasForm = this.fb2.group({
+      contenido: new FormControl({ value: null, disabled: false }, [Validators.required]),
+      author_id: new FormControl({ value: null, disabled: false }, [Validators.required]),
+      fecha_difusion: new FormControl({ value: null, disabled: false }, [Validators.required])
     });
   }
 }
